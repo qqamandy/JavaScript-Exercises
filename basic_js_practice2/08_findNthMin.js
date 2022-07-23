ecb5942524e16e70cf220f20e3244e9eeb46a09a
mandy <qqamandy@gmail.com>
2022-07-23 11:59:10 +0800

    intermediate added

--- basic_js_practice2/08_findNthMin.js
+++ basic_js_practice2/08_findNthMin.js
@@ -4,7 +4,18 @@
 // findNthMin([1, 5, 15, 7, 9], 3); // returns 7
 
 function findNthMin(arr,n){
-    let result = []
+    for(let i = 0; i < arr.length; i++){
+        let count = 0
+        for(let j = 0; j <arr.length; j++){
+            if(arr[j]<arr[i]){
+                count++
+            }
+        }
+        if(count == n-1){
+            return arr[i]
+        }
+    }
+
     
 }
 
